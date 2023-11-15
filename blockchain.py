0bdd983d744a057726d58060bdf092c377ee19b0
Mennah Ayman <101893442+MennahAymann@users.noreply.github.com>
2023-11-15 14:37:11 +0200

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -44,3 +44,19 @@ class Blockchain:
     def chain_valid(self, chain):
         previous_block = chain[0]
         block_index = 1
+      while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(
+                str(proof*2 - previous_proof*2).encode()).hexdigest()
+
+            if hash_operation[:5] != '00000':
+                return False
+            previous_block = block
+            block_index += 1
+
+        return True
